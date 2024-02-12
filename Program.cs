f503144dcf657a5e6330c4c0efd812ab32630c67
Syed Shah <Syed.Shah@lancashiregroup.com>
2024-02-12 12:44:28 +0000

    Configure DB & Redis & add migration bundle

--- Program.cs
+++ Program.cs
@@ -9,7 +9,13 @@ builder.Logging.AddAzureWebAppDiagnostics();
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlite("Data Source=localdatabase.db"));
+                    options.UseSqlServer("AZURE_SQL_CONNECTIONSTRING"));
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
+    options.InstanceName = "SampleInstance";
+});
 
 var app = builder.Build();
 
